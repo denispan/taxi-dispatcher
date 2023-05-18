11050e8f5c9fd17e22cb5b73601bce422b135412
denispan <6231990@gmail.com>
2023-05-18 21:29:35 +0300

    мелкие исправления

--- src/components/AddCar/AddCar.jsx
+++ src/components/AddCar/AddCar.jsx
@@ -1,18 +1,64 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from "./AddCar.module.scss";
-
-
+import cx from 'classnames';
 
 const AddCar = () => {
   const [isOpenAddCar, setIsOpenAddCar] = useState(false);
+  const addCarRef = useRef(null);
+  const btnRef = useRef(null);
 
   const onClickAddCarHandler = () => {
     setIsOpenAddCar(!isOpenAddCar);
   }
 
+// Hook
+function useOnClickOutside(refPopup, refBtn, handler) {
+  useEffect(() => {
+      const listener = (event) => {
+        // Do nothing if clicking ref's element or descendent elements
+        if (!refPopup.current || refPopup.current.contains(event.target) || event.target === refBtn.current) {
+          return;
+        }
+        handler(event);
+      };
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+      return () => {
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
+    },
+    [refPopup, refBtn, handler]);
+}
+
+  // Call hook passing in the ref and a function to call on outside click
+  useOnClickOutside(addCarRef, btnRef, () => setIsOpenAddCar(false));
+
   return (
     <>
-      <button onClick={onClickAddCarHandler}>Добавить автомобиль</button>
+      <button
+        ref={btnRef}
+        className={cx(styles.btn, isOpenAddCar ? styles.active : '')}
+        onClick={onClickAddCarHandler}
+      >
+        Добавить автомобиль
+      </button>
+      {
+        isOpenAddCar && (
+          <div ref={addCarRef} className={styles.add_car_popup}>
+            <form action="">
+              <input type="text" placeholder={'Номер машины'}/>
+              <input type="text" placeholder={'Имя водителя'}/>
+              <input type="text" placeholder={'Телефон водителя'}/>
+              <div className={styles.btns}>
+                <button className={styles.btn} type={'submit'}>Отправить</button>
+                <button className={cx(styles.btn, styles.btn_reset)} type={'reset'}>Очистить</button>
+              </div>
+
+            </form>
+          </div>
+        )
+      }
     </>
   )
 };
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,29 +1,123 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from "./Orders.module.scss";
+import cx from "classnames";
 
+const ordersList = [
+  {
+    id: '1',
+    name: 'Кирилл',
+    phone: '+79031234567',
+    status: 'Ожидает'
+  },
+  {
+    id: '2',
+    name: 'Андрейcdv df fd vdf fd df df dfv fdvdf',
+    phone: '+79033333333',
+    status: 'Принят'
+  },
+  {
+    id: '3',
+    name: 'Михаил',
+    phone: '+79034444444',
+    status: 'Отменен'
+  },
+  {
+    id: '4',
+    name: 'Маргарита',
+    phone: '+79034444в444',
+    status: 'Принят'
+  },
+  {
+    id: '5',
+    name: 'Маргарита',
+    phone: '+79034444в444',
+    status: 'Принят'
+  },
+  {
+    id: '6',
+    name: 'Маргарита',
+    phone: '+79034444в444',
+    status: 'Принят'
+  },
+  {
+    id: '7',
+    name: 'Маргарита',
+    phone: '+79034444в444',
+    status: 'Принят'
+  },
+  {
+    id: '8',
+    name: 'Маргарита',
+    phone: '+79034444в444',
+    status: 'Принят'
+  },
+];
 
-const Orders = (ordersList) => {
+const Orders = () => {
   const [isOpenOrders, setIsOpenOrders] = useState(false);
+
+  const ordersRef = useRef(null);
+  const btnRef = useRef(null);
+
+// Hook
+  function useOnClickOutside(refPopup, refBtn, handler) {
+    useEffect(() => {
+        const listener = (event) => {
+          // Do nothing if clicking ref's element or descendent elements
+          if (!refPopup.current || refPopup.current.contains(event.target) || event.target === refBtn.current) {
+            return;
+          }
+          handler(event);
+        };
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+        return () => {
+          document.removeEventListener("mousedown", listener);
+          document.removeEventListener("touchstart", listener);
+        };
+      },
+      [refPopup, btnRef, handler]);
+  }
+
+  // Call hook passing in the ref and a function to call on outside click
+  useOnClickOutside(ordersRef, btnRef, () => setIsOpenOrders(false));
 
   const onClickOrdersHandler = () => {
     setIsOpenOrders(!isOpenOrders);
-  }
+  };
 
   return (
     <>
-      <button className={'btn'} onClick={onClickOrdersHandler}>История заказов</button>
+      <button ref={btnRef} className={cx(styles.btn, isOpenOrders ? styles.active : '')} onClick={onClickOrdersHandler}>История заказов</button>
       { isOpenOrders &&
-        (<div className={styles.orders_popup}>
-          <ul>
-            {/*{*/}
-            {/*  ordersList.map((orderItem, index) =>*/}
-            {/*  <li key={index}*/}
-            {/*      className={styles.orders_item}*/}
-            {/*  >*/}
-            {/*    {orderItem.orderName}*/}
-            {/*  </li>*/}
-            {/*)}*/}
-          </ul>
+        (<div ref={ordersRef} className={styles.orders_popup}>
+          <div className={styles.orders_list}>
+            <table>
+              <thead>
+                <tr>
+                  <th className={styles.orders_id}>Номер заказа</th>
+                  <th className={styles.orders_name}>Имя клиента</th>
+                  <th className={styles.orders_phone}>Телефон клиента</th>
+                  <th className={styles.orders_status}>Статус заказа</th>
+                </tr>
+              </thead>
+            </table>
+            <div className={styles.orders_list_scroll}>
+              <table>
+                <tbody>
+                {
+                  ordersList.map((orderItem, index) =>
+                    <tr key={index}>
+                      <td className={styles.orders_id}>{orderItem.id}</td>
+                      <td className={styles.orders_name}>{orderItem.name}</td>
+                      <td className={styles.orders_phone}>{orderItem.phone}</td>
+                      <td className={styles.orders_status}>{orderItem.status}</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>)}
     </>
   )
